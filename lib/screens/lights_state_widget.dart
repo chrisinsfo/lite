94d74af941955bd7283f6e43809adf79f35f05fb
Chris Wardell <ctwardell@me.com>
2022-03-22 13:37:39 -0700

    Add header row

--- lib/screens/lights_state_widget.dart
+++ lib/screens/lights_state_widget.dart
@@ -34,42 +34,55 @@ class _LightsStateWidgetState extends State<LightsStateWidget> {
   Widget build(BuildContext context) {
     final lights = Provider.of<LightsBloc>(context, listen: false);
 
-    return Container(
-      height: MediaQuery.of(context).size.height - 50.0,
-      child: StreamBuilder(
-        stream: lights.lightStream,
-        builder: (ctx, snapshot) {
-          if (snapshot.hasError) {
-            print('error.log: ${snapshot.error.toString()}');
-          }
-          if (!snapshot.hasData) {
-            return Text("No Data");
-          }
-          if (snapshot.hasData) {
-            List<LightModel> lights = snapshot.data as List<LightModel>;
-            if (lights.length == 0) {
-              return Text("No lights to show");
-            };
-            List<LightSwitchWidget> lightList = List.empty(growable: true);
-            for(var light in lights) {
-              lightList.add(LightSwitchWidget(light));
-            }
-            return Container(
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                children: lightList,
-              ),
-            );
-          }
-          return Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Align(
-              alignment: Alignment.center,
-              child: CircularProgressIndicator(),
-            ),
-          );
-        },
-      ),
+    return Column(
+      children: [
+        Row(
+          children: const [
+            Text("id", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+            Spacer(),
+            Text("Name", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+            Spacer(),
+            Text("State", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold))
+          ],
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height - 50.0,
+          child: StreamBuilder(
+            stream: lights.lightStream,
+            builder: (ctx, snapshot) {
+              if (snapshot.hasError) {
+                print('error.log: ${snapshot.error.toString()}');
+              }
+              if (!snapshot.hasData) {
+                return Text("No Data");
+              }
+              if (snapshot.hasData) {
+                List<LightModel> lights = snapshot.data as List<LightModel>;
+                if (lights.length == 0) {
+                  return Text("No lights to show");
+                };
+                List<LightSwitchWidget> lightList = List.empty(growable: true);
+                for(var light in lights) {
+                  lightList.add(LightSwitchWidget(light));
+                }
+                return Container(
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    children: lightList,
+                  ),
+                );
+              }
+              return Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
