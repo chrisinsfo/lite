278d5c0128384259d14e2711f9e16dcbc4fec8af
Chris Wardell <ctwardell@me.com>
2022-03-28 18:37:57 -0700

    Update design properties

--- lib/widgets/device_list_tile.dart
+++ lib/widgets/device_list_tile.dart
@@ -12,12 +12,19 @@ class DeviceListTile extends StatelessWidget {
     final devices = Provider.of<DeviceBloc>(context, listen: false);
     final bool onState = devices.lightsStateCache[deviceModel.id] ?? false;
 
-    return ListTile(
-      leading: const Icon(Icons.network_wifi, color: Colors.yellow),
-      title: Text(deviceModel.metadata.name, style: const TextStyle(color: Colors.white)),
-      subtitle: Text(deviceModel.metadata.archetype, style: const TextStyle(color: Colors.white70)),
-      //TODO: implement onChanged
-      trailing: Switch(value: onState, onChanged: null),
+    return Card(
+      color: Colors.black12,
+      child: ListTile(
+        leading: const Icon(Icons.network_wifi, color: Colors.yellow),
+        title: Text(deviceModel.metadata.name, style: const TextStyle(color: Colors.white)),
+        subtitle: Text(deviceModel.metadata.archetype, style: const TextStyle(color: Colors.white70)),
+        //TODO: implement onChanged
+        trailing: Switch(
+            value: onState,
+            onChanged: null,
+            trackColor: onState == true ? MaterialStateProperty.all(Colors.green) : MaterialStateProperty.all(Colors.orange),
+        ),
+      ),
     );
   }
 }
