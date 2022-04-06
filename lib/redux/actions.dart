4ac5457ee6e5d93c592b47013480eea3ea3a1b31
Chris Wardell <ctwardell@me.com>
2022-04-06 11:42:56 -0700

    Extend actions for Device List and Lights State

--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -1,7 +1,119 @@
+import 'dart:convert';
+
 import 'package:lite/models/model.dart';
+import 'package:http/http.dart' as http;
+import 'package:lite/models/device_model.dart';
 
 class SetConfigAction {
   final Config config;
 
   SetConfigAction(this.config);
+}
+
+class ValidateConfigAction {
+  bool isValidConfig(Config config) {
+    return config.ipAddress.isNotEmpty
+        && !(config.ipAddress == 'Unknown')
+        && config.username.isNotEmpty
+        && !(config.username == 'Unknown');
+  }
+}
+
+class ToggleLightAction {
+  final Config _config;
+  final Map<String, bool> lightsStateCache;
+  ToggleLightAction(this._config, this.lightsStateCache);
+
+  Future toggleLight(String lightId) async {
+    final ip = _config.ipAddress;
+    final applicationKey = _config.username;
+    var state = lightsStateCache[lightId];
+
+    final Uri uri = Uri.parse('https://$ip/clip/v2/resource/light/$lightId');
+    var body = json.encode({
+      "on": {"on" : state}
+    });
+
+    final Map<String, String> headers = { 'hue-application-key' : applicationKey };
+
+    try {
+      await http.put(uri, body: body, headers: headers);
+      if (state != null) {
+        lightsStateCache[lightId] = state;
+      }
+    } catch (error) {
+      print(error);
+    }
+  }
+}
+
+class GetLightsStateAction {
+  final Map<String, bool> lightsStateCache = Map<String, bool>();
+
+  final Config _config;
+  GetLightsStateAction(this._config);
+
+    Future getLightsState() async {
+      final ip = _config.ipAddress;
+      final applicationKey = _config.username;
+
+      final Uri uri = Uri.parse('https://$ip/clip/v2/resource/light');
+
+      final Map<String, String> headers = { 'hue-application-key' : applicationKey };
+
+      late var response;
+
+      try {
+        response = await http.get(uri, headers: headers);
+      } catch(error) {
+        print(error);
+      }
+
+      if (response.statusCode == 200) {
+        final decoded = jsonDecode(response.body);
+        final lights = decoded['data'];
+        for(var light in lights) {
+          lightsStateCache[light['owner']['rid']] = light['on']['on'];
+        }
+
+      } else {
+        // TODO: follow API guidelines for error handling
+        final errors = jsonDecode(response.body);
+        throw(errors['errors']);
+      }
+    }
+}
+
+class GetDevicesAction {
+  final Config config;
+  List<DeviceModel> deviceList;
+
+  GetDevicesAction(this.config, this.deviceList);
+
+  Future getDevices() async {
+    final ip = config.ipAddress;
+    final applicationKey = config.username;
+
+    final Uri uri = Uri.parse('https://$ip/clip/v2/resource/device');
+
+    final Map<String, String> headers = { 'hue-application-key' : applicationKey };
+
+    late var response;
+
+    try {
+      response = await http.get(uri, headers: headers);
+    } catch(error) {
+      print(error);
+    }
+
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body);
+      deviceList = DeviceModel.deserialize(decoded['data']);
+    } else {
+      // TODO: follow API guidelines for error handling
+      final errors = jsonDecode(response.body);
+      throw(errors['errors']);
+    }
+  }
+
 }
