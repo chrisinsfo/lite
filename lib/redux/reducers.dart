1ad2fb6c89223da4bef3d7ac5eb48035689c93f1
Chris Wardell <ctwardell@me.com>
2022-04-07 22:49:39 -0700

    Reducer responsible for validating config

--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -3,10 +3,18 @@ import 'package:lite/redux/actions.dart';
 
 // called every time we want to commit a change to store
 AppState appStateReducer(AppState state, action) {
+
   if (action is SetConfigAction) {
     SetConfigAction a = action;
-    return AppState(a.config);
-  } else {
-    return state;
+    return AppState(
+        Config(
+            a.config.ipAddress,
+            a.config.username,
+            a.config.ipAddress.isNotEmpty
+                && !(a.config.ipAddress == 'Unknown')
+                && a.config.username.isNotEmpty
+                && !(a.config.username == 'Unknown'))
+    );
   }
+  return state;
 }
