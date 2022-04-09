f57e8be026ceac80674d62f9c95256d94aef08ca
Chris Wardell <ctwardell@me.com>
2022-04-08 22:08:22 -0700

    Fix switch toggle with local onState

--- lib/widgets/device_list_tile.dart
+++ lib/widgets/device_list_tile.dart
@@ -3,41 +3,49 @@ import 'package:lite/models/device_model.dart';
 
 class DeviceListTile extends StatefulWidget {
   final DeviceModel deviceModel;
-  bool onState;
+  bool initialOnState;
   Function(String) toggleOnState;
 
-  DeviceListTile(this.deviceModel, this.onState, this.toggleOnState);
+  DeviceListTile(this.deviceModel, this.initialOnState, this.toggleOnState);
 
   @override
   State<DeviceListTile> createState() => _DeviceListTileState();
 }
 
 class _DeviceListTileState extends State<DeviceListTile> {
-  //TODO: local state
+
+  bool onState = false;
+
+  @override
+  void initState() {
+    onState = widget.initialOnState;
+    super.initState();
+  }
   
   @override
   Widget build(BuildContext context) {
+    print('build DeviceListTile');
     return Card(
         color: Colors.black12,
         child: ListTile(
           leading: const Icon(Icons.network_wifi, color: Colors.yellow),
           title: Text(widget.deviceModel.metadata.name, style: const TextStyle(color: Colors.white)),
           subtitle: Text(widget.deviceModel.metadata.archetype.replaceAll('_', ' '), style: const TextStyle(color: Colors.white70)),
-          trailing: widget.onState
+          trailing: onState
               ? Switch(
-              value: widget.onState,
+              value: onState,
               onChanged: (bool newValue) {
                 setState(() {
-                  widget.onState = newValue;
+                  onState = newValue;
                   widget.toggleOnState(widget.deviceModel.services.first.rid);
                 });
               },
               trackColor: MaterialStateProperty.all(Colors.green))
               : Switch(
-              value: widget.onState,
+              value: onState,
               onChanged: (bool newValue) {
                 setState(() {
-                  widget.onState = newValue;
+                  onState = newValue;
                   widget.toggleOnState(widget.deviceModel.services.first.rid);
                 });
               },
