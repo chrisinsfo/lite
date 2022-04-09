830067491216fc4f722bf06813ddf9957bc25a46
Chris Wardell <ctwardell@me.com>
2022-04-08 18:35:29 -0700

    Adapt DeviceListScreen to Redux with DevicesContainer

---
+++ lib/containers/devices_container.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:lite/models/model.dart';
+import 'package:lite/redux/actions.dart';
+import 'package:lite/models/device_model.dart';
+import 'package:lite/screens/device_list_screen.dart';
+import 'package:redux/redux.dart';
+
+class DevicesContainer extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, _ViewModel>(
+      // convert the store into a viewModel
+      // the resulting viewModel will be passed into the builder function
+        converter: _ViewModel.fromStore,
+        builder: (context, vm) =>
+            DeviceListScreen(vm.deviceList, vm.config, vm.lightsStateCache)
+    );
+  }
+}
+
+class _ViewModel {
+  List<DeviceModel> deviceList;
+  Config config;
+  Map<String, bool> lightsStateCache;
+
+  _ViewModel(this.deviceList, this.config, this.lightsStateCache);
+
+  static _ViewModel fromStore(Store<AppState> store) {
+    if (store.state.applicationState == ApplicationState.validConfig) {
+      store.dispatch(GetDevicesAction(store.state.config, store.state.deviceList));
+    }
+    return _ViewModel(store.state.deviceList, store.state.config, store.state.lightsStateCache);
+  }
+}
--- lib/widgets/device_list_tile.dart
+++ lib/widgets/device_list_tile.dart
@@ -1,64 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
-
-import 'package:lite/blocs/device_bloc.dart';
 import 'package:lite/models/device_model.dart';
-import 'package:lite/models/model.dart';
-import 'package:provider/provider.dart';
 
 class DeviceListTile extends StatefulWidget {
-  const DeviceListTile(this.deviceModel);
   final DeviceModel deviceModel;
+  var onState = false;
+  Function toggleOnState;
+
+  DeviceListTile(this.deviceModel, this.onState, this.toggleOnState);
 
   @override
   State<DeviceListTile> createState() => _DeviceListTileState();
 }
 
 class _DeviceListTileState extends State<DeviceListTile> {
-  var onState = false;
-
-  @override
-  void initState() {
-    final devices = Provider.of<DeviceBloc>(context, listen: false);
-    onState = devices.lightsStateCache[widget.deviceModel.id] ?? false;
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    final deviceBloc = Provider.of<DeviceBloc>(context, listen: false);
-    return StoreConnector<AppState, Config>(
-      converter: (Store<AppState> store) => store.state.config,
-      builder: (context, config) => Card(
+    return Card(
         color: Colors.black12,
         child: ListTile(
           leading: const Icon(Icons.network_wifi, color: Colors.yellow),
           title: Text(widget.deviceModel.metadata.name, style: const TextStyle(color: Colors.white)),
           subtitle: Text(widget.deviceModel.metadata.archetype.replaceAll('_', ' '), style: const TextStyle(color: Colors.white70)),
-          trailing: onState
+          trailing: widget.onState
               ? Switch(
-              value: onState,
+              value: widget.onState,
               onChanged: (bool newValue) {
                 setState(() {
-                  onState = newValue;
-
-                  deviceBloc.toggleLight(context, config, widget.deviceModel.services.first.rid, onState);
+                  widget.onState = newValue;
+                  //TODO: toggleLight
+                  // deviceBloc.toggleLight(context, config, widget.deviceModel.services.first.rid, onState);
                 });
               },
               trackColor: MaterialStateProperty.all(Colors.green))
               : Switch(
-              value: onState,
+              value: widget.onState,
               onChanged: (bool newValue) {
                 setState(() {
-                  onState = newValue;
-                  deviceBloc.toggleLight(context, config, widget.deviceModel.services.first.rid, onState);
+                  widget.onState = newValue;
+                  //TODO: toggleLight
+                  // deviceBloc.toggleLight(context, config, widget.deviceModel.services.first.rid, onState);
                 });
               },
               trackColor: MaterialStateProperty.all(Colors.orange)
           ),
         ),
-      ),
-    );
+      );
   }
 }
