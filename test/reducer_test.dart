199f9cae65993eec25381c0218a7cacd1ad8b014
Chris Wardell <ctwardell@me.com>
2022-04-11 11:17:14 -0700

    Implement Devices and LightsState reducer tests

--- test/reducer_test.dart
+++ test/reducer_test.dart
@@ -1,19 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lite/models/device_model.dart';
 import 'package:lite/models/model.dart';
 import 'package:redux/redux.dart';
 import 'package:lite/redux/reducers.dart';
 import 'package:lite/redux/actions.dart';
 
 void main() {
-  group('State Reducer', ()
-  {
+  group('State Reducer', () {
     test('should update store config to valid with valid inputs', () {
       final store = Store<AppState>(
         appStateReducer,
         initialState: AppState.initialState(),
       );
 
-      store.dispatch(WillSetConfigAction(Config('123.456.78.9', 'Mark', false)));
+      store
+          .dispatch(WillSetConfigAction(Config('123.456.78.9', 'Mark', false)));
 
       expect(store.state.config.isValid, true);
       expect(store.state.config.username, 'Mark');
@@ -29,6 +30,60 @@ void main() {
       store.dispatch(WillSetConfigAction(Config('Unknown', 'Mark', false)));
 
       expect(store.state.config.isValid, false);
+    });
+
+    test('GetDevicesAction should update applicationState to ApplicationState.fetchingDevices', () {
+      final store = Store<AppState>(
+        appStateReducer,
+        initialState: AppState.initialState(),
+      );
+
+      store.dispatch(GetDevicesAction(DeviceModel.copyWith(store.state.deviceList)));
+
+      expect(store.state.applicationState, ApplicationState.fetchingDevices);
+    });
+
+    test('FetchedDevicesAction should update applicationState to .fetchedDevices and update state device list', () {
+      final store = Store<AppState>(
+        appStateReducer,
+        initialState: AppState.initialState(),
+      );
+
+      final devices = <DeviceModel>[
+        DeviceModel('1234', Metadata('spot_bulb', 'LR1'),
+            <Service>[Service("1234", "light")])
+      ];
+
+      store.dispatch(FetchedDevicesAction(devices));
+
+      expect(store.state.applicationState, ApplicationState.fetchedDevices);
+      expect(store.state.deviceList, devices);
+    });
+
+    test('GetLightsStateAction should update applicationState to ApplicationState.fetchingLightsState', () {
+      final store = Store<AppState>(
+        appStateReducer,
+        initialState: AppState.initialState(),
+      );
+
+      store.dispatch(GetLightsStateAction(store.state.lightsStateCache));
+
+      expect(store.state.applicationState, ApplicationState.fetchingLightsState);
+    });
+
+    test('FetchedLightsStateAction should update applicationState to ApplicationState.fetchedLightsState '
+        'and update state.lightsStateCache' , () {
+      final store = Store<AppState>(
+        appStateReducer,
+        initialState: AppState.initialState(),
+      );
+
+      final lightsStateCache = {'12345': false};
+
+      store.dispatch(FetchedLightsStateAction(lightsStateCache));
+
+      expect(store.state.applicationState, ApplicationState.fetchedLightsState);
+      expect(store.state.lightsStateCache, lightsStateCache);
     });
 
   });
