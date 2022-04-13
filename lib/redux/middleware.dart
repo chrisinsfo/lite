e2ca714e82e5df7cb0dffcb24d066ff58e1fb41c
Chris Wardell <ctwardell@me.com>
2022-04-12 18:25:55 -0700

    Replace homespun middleware with ReduxThunk for getDevices and getLightsState

--- lib/redux/middleware.dart
+++ lib/redux/middleware.dart
@@ -7,77 +7,67 @@ import 'package:lite/models/device_model.dart';
 import 'package:lite/redux/actions.dart';
 import 'package:redux/redux.dart';
 
-void deviceApiMiddleware(Store<AppState> store, dynamic action, NextDispatcher next) {
+void getDevices (Store<AppState> store) async {
+  List<DeviceModel> deviceList;
+  final ip = store.state.config.ipAddress;
+  final applicationKey = store.state.config.username;
 
-  void getDevices() async {
-    List<DeviceModel> deviceList;
-    final GetDevicesAction a = action;
-    final ip = store.state.config.ipAddress;
-    final applicationKey = store.state.config.username;
+  final Uri uri = Uri.parse('https://$ip/clip/v2/resource/device');
 
-    final Uri uri = Uri.parse('https://$ip/clip/v2/resource/device');
+  final Map<String, String> headers = { 'hue-application-key' : applicationKey };
 
-    final Map<String, String> headers = { 'hue-application-key' : applicationKey };
+  late var response;
 
-    late var response;
-
-    try {
-      response = await http.get(uri, headers: headers);
-    } catch(error) {
-      developer.log('middleware', error: jsonEncode(error));
-    }
-
-    if (response.statusCode == 200) {
-      final decoded = jsonDecode(response.body);
-      deviceList = DeviceModel.deserialize(decoded['data']);
-      store.dispatch(FetchedDevicesAction(deviceList));
-    } else {
-      // TODO: follow API guidelines for error handling
-      // TODO: implement and dispatch DevicesNotLoadedAction
-      final errors = jsonDecode(response.body);
-      throw(errors['errors']);
-    }
+  try {
+    response = await http.get(uri, headers: headers);
+  } catch(error) {
+    developer.log('middleware', error: jsonEncode(error));
   }
 
-  if (action is GetDevicesAction) {
-    getDevices();
+  if (response.statusCode == 200) {
+    final decoded = jsonDecode(response.body);
+    deviceList = DeviceModel.deserialize(decoded['data']);
+    store.dispatch(FetchedDevicesAction(deviceList));
+  } else {
+    // TODO: follow API guidelines for error handling
+    // TODO: implement and dispatch DevicesNotLoadedAction
+    final errors = jsonDecode(response.body);
+    throw(errors['errors']);
+  }
+}
+
+void getLightsState(Store<AppState> store) async {
+  final Map<String, bool> lightsStateCache = <String, bool>{};
+  final ip = store.state.config.ipAddress;
+  final applicationKey = store.state.config.username;
+
+  final Uri uri = Uri.parse('https://$ip/clip/v2/resource/light');
+
+  final Map<String, String> headers = { 'hue-application-key' : applicationKey };
+
+  late var response;
+
+  try {
+    response = await http.get(uri, headers: headers);
+  } catch(error) {
+    developer.log('middleware', error: jsonEncode(error));
   }
 
-  next(action);
+  if (response.statusCode == 200) {
+    final decoded = jsonDecode(response.body);
+    final lights = decoded['data'];
+    for(var light in lights) {
+      lightsStateCache[light['owner']['rid']] = light['on']['on'];
+    }
+    store.dispatch(FetchedLightsStateAction(lightsStateCache));
+  } else {
+    // TODO: follow API guidelines for error handling
+    final errors = jsonDecode(response.body);
+    throw(errors['errors']);
+  }
 }
 
 void lightsApiMiddleware(Store<AppState> store, dynamic action, NextDispatcher next) {
-
-  void getLightsState() async {
-    final Map<String, bool> lightsStateCache = <String, bool>{};
-    final ip = store.state.config.ipAddress;
-    final applicationKey = store.state.config.username;
-
-    final Uri uri = Uri.parse('https://$ip/clip/v2/resource/light');
-
-    final Map<String, String> headers = { 'hue-application-key' : applicationKey };
-
-    late var response;
-
-    try {
-      response = await http.get(uri, headers: headers);
-    } catch(error) {
-      developer.log('middleware', error: jsonEncode(error));
-    }
-
-    if (response.statusCode == 200) {
-      final decoded = jsonDecode(response.body);
-      final lights = decoded['data'];
-      for(var light in lights) {
-        lightsStateCache[light['owner']['rid']] = light['on']['on'];
-      }
-      store.dispatch(FetchedLightsStateAction(lightsStateCache));
-    } else {
-      // TODO: follow API guidelines for error handling
-      final errors = jsonDecode(response.body);
-      throw(errors['errors']);
-    }
-  }
 
   void toggleLight(String lightId) async {
     final ip = store.state.config.ipAddress;
@@ -108,11 +98,6 @@ void lightsApiMiddleware(Store<AppState> store, dynamic action, NextDispatcher n
       final errors = jsonDecode(response.body);
       throw(errors['errors']);
     }
-  }
-
-  if (action is GetLightsStateAction) {
-    developer.log('middleware: GetLightsStateAction');
-    getLightsState();
   }
 
   if (action is ToggleLightAction) {
